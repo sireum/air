d24b715aeb202713668d0f8c4721db62f92dc658
Robby <robby@k-state.edu>
2018-03-20 15:07:33 -0500

    Reorganized CLI groups.

--- shared/src/main/scala/org/sireum/aadl/ir/regen.sh
+++ shared/src/main/scala/org/sireum/aadl/ir/regen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 : ${SIREUM_HOME:=$( cd "$( dirname "$0" )"/../../../../../../../../../../.. &> /dev/null && pwd )}
-$SIREUM_HOME/platform/java/bin/java -jar $SIREUM_HOME/bin/sireum.jar util sergen -l $SIREUM_HOME/license.txt -m json,msgpack AadlAST.scala
+$SIREUM_HOME/platform/java/bin/java -jar $SIREUM_HOME/bin/sireum.jar tools sergen -l $SIREUM_HOME/license.txt -m json,msgpack AadlAST.scala

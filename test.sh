be3ad9b5f9258908176cf092093f103b711ff1af
Robby <robby@k-state.edu>
2018-10-15 01:56:17 -0500

    Cleaned up.

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-$SCRIPT_HOME/prelude.sh
+cd $SCRIPT_HOME
+./prelude.sh
 rm -fR runtime
 git clone --depth=1 https://github.com/sireum/runtime
-$SCRIPT_HOME/sireum slang tipe --verbose -r -s runtime/library:shared:jvm
-$SCRIPT_HOME/mill-standalone air.shared.tests
+./sireum slang tipe --verbose -r -s runtime/library:shared:jvm
+./mill-standalone air.shared.tests
 if [ -x "$(command -v node)" ]; then
-  $SCRIPT_HOME/mill-standalone air.js.tests
+  ./mill-standalone air.js.tests
 fi

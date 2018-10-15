c21df52a344e17ecc4ef146b07c9d9a282ffa210
Robby <robby@k-state.edu>
2018-10-15 10:49:28 -0500

    Removed out in test.sh first.

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 cd $SCRIPT_HOME
 ./prelude.sh
 ./sireum slang tipe --verbose -r -s runtime/library:shared:jvm
+rm -fR out
 ./mill-standalone air.shared.tests
 if [ -x "$(command -v node)" ]; then
   ./mill-standalone air.js.tests

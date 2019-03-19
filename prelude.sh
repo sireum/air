a6beeac7f17064033e6d19216b347cd64f688f04
Jason Belt <belt@ksu.edu>
2019-03-19 15:15:02 -0500

    update prelude

--- prelude.sh
+++ prelude.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 cd ${SCRIPT_HOME}
+CURL='curl -c /dev/null -JLso'
 if [ ! -e sireum ]; then
-  curl -Lo sireum http://files.sireum.org/sireum
+  ${CURL} sireum http://files.sireum.org/sireum
   chmod +x sireum
 fi
 if [ ! -e mill-standalone ]; then
-  curl -Lo mill-standalone http://files.sireum.org/mill-standalone
+  ${CURL} mill-standalone http://files.sireum.org/mill-standalone
   chmod +x mill-standalone
 fi
 if [ ! -e versions.properties ]; then
-  curl -Lo versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
+  ${CURL} versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
 fi
 rm -fR runtime
 git clone --depth=1 https://github.com/sireum/runtime

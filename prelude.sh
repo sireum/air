1f9da158e1e43c4cb0a525af96d563d2284fc368
Robby <robby@santoslab.org>
2021-08-20 07:45:35 -0500

    Updated mill download URL.

--- prelude.sh
+++ prelude.sh
@@ -7,7 +7,7 @@ if [ ! -e sireum ]; then
   chmod +x sireum
 fi
 if [ ! -e mill-standalone ]; then
-  ${CURL} mill-standalone https://github.com/sireum/releases/releases/download/mill/mill
+  ${CURL} mill-standalone https://github.com/sireum/rolling/releases/download/mill/standalone
   chmod +x mill-standalone
 fi
 if [ ! -e versions.properties ]; then
